8232d8cfe01be40cfc33e9f77f5037d79eaf2cc8
Eirik Steira <steiraeirik@gmail.com>
2023-04-27 12:35:47 +0200

    Add class Processor

---
+++ src/processors.py
@@ -0,0 +1,9 @@
+class Processor:
+    def __init__(self):
+        self.db_path_or_url = ""
+
+    def get_db_path_or_url(self):
+        return self.db_path_or_url
+    
+    def set_db_path_or_url(self, db_path_or_url):
+        self.db_path_or_url = db_path_or_url
